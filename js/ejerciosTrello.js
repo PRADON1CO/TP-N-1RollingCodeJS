af1b7c2cd3aad006143f19cd0d93deca539b745b
Prado Nicolas <nicolas88prado@gmail.com>
2023-12-26 20:13:58 -0300

    realice el ejercicio N1 de trello

---
+++ js/ejerciosTrello.js
@@ -0,0 +1,8 @@
+//Ejercicio 1
+let num1 = 60;
+let num2 = 10;
+
+document.write('El resultado de sumar ' + num1 + ' y ' + num2 + ' es: ' + (num1 + num2) );
+document.write('<br>El resultado de restar ' + num1 + ' y ' + num2 + ' es: ' + (num1 - num2) );
+document.write('<br>El resultado de hacer el producto entre  ' + num1 + ' y ' + num2 + ' es: ' + (num1 * num2) );
+document.write('<br>El resultado de dividir ' + num1 + ' y ' + num2 + ' es: ' + (num1 / num2) );
