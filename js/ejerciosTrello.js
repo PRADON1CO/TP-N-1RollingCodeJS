ba60f65bfdd9bc144bdb43ca270c2c0b6073136e
Prado Nicolas <nicolas88prado@gmail.com>
2023-12-26 20:20:58 -0300

    realice el ejercio N2 de trello

--- js/ejerciosTrello.js
+++ js/ejerciosTrello.js
@@ -6,3 +6,9 @@ document.write('El resultado de sumar ' + num1 + ' y ' + num2 + ' es: ' + (num1 
 document.write('<br>El resultado de restar ' + num1 + ' y ' + num2 + ' es: ' + (num1 - num2) );
 document.write('<br>El resultado de hacer el producto entre  ' + num1 + ' y ' + num2 + ' es: ' + (num1 * num2) );
 document.write('<br>El resultado de dividir ' + num1 + ' y ' + num2 + ' es: ' + (num1 / num2) );
+
+//Ejercicio 2
+let nombre = ' Brian Nicolas ';
+let apellido = ' Prado ';
+let nombreCompleto = apellido + nombre;
+document.write('<br>' + nombreCompleto)
