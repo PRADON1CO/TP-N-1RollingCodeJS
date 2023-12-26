e097997ba7baae87eb09f12da7f6f1686859f9d2
Prado Nicolas <nicolas88prado@gmail.com>
2023-12-26 20:31:32 -0300

    Realice el ejercicio N3 de trello

--- js/ejerciosTrello.js
+++ js/ejerciosTrello.js
@@ -5,10 +5,15 @@ let num2 = 10;
 document.write('El resultado de sumar ' + num1 + ' y ' + num2 + ' es: ' + (num1 + num2) );
 document.write('<br>El resultado de restar ' + num1 + ' y ' + num2 + ' es: ' + (num1 - num2) );
 document.write('<br>El resultado de hacer el producto entre  ' + num1 + ' y ' + num2 + ' es: ' + (num1 * num2) );
-document.write('<br>El resultado de dividir ' + num1 + ' y ' + num2 + ' es: ' + (num1 / num2) );
+document.write('<br>El resultado de dividir ' + num1 + ' y ' + num2 + ' es: ' + (num1 / num2) + '<br>');
 
 //Ejercicio 2
 let nombre = ' Brian Nicolas ';
 let apellido = ' Prado ';
 let nombreCompleto = apellido + nombre;
-document.write('<br>' + nombreCompleto)
+document.write('<br>' + nombreCompleto + '<br>');
+
+//ejercicio 3
+let gradosCelsius = 6;
+let gradosFahrenheit = ((gradosCelsius * 9) / 5 + 32);
+document.write('<br>La tempura en grados celcius es ' + gradosCelsius + '°C' +' y la temperatura en Fahrenheit es: '+ gradosFahrenheit + ' °F');
