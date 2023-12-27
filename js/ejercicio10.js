e0aa01ccf8b81d2adb4eb059c7da4f8092c3b719
Prado Nicolas <nicolas88prado@gmail.com>
2023-12-27 12:20:03 -0300

    corregit nombre de la variable numeroA para las condicionales y agregar src en el index

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -1,13 +1,13 @@
 let numeroA = parseInt(prompt("Ingrese un número: "));
 
-if (numero % 2 === 0) {
-    document.write('<br>El ' + numeroA + 'es divisible por 2.');
-} else if (numero % 3 === 0) {
-    document.write('<br>El ' + numeroA + 'es divisible por 3.');
-} else if (numero % 5 === 0) {
-    document.write('<br>El ' + numeroA + 'es divisible por 5.');
-} else if (numero % 7 === 0) {
-    document.write('<br>El ' + numeroA + 'es divisible por 7.');
+if (numeroA % 2 === 0) {
+    document.write('<br>El ' + numeroA + ' es divisible por 2.');
+} else if (numeroA % 3 === 0) {
+    document.write('<br>El ' + numeroA + ' es divisible por 3.');
+} else if (numeroA % 5 === 0) {
+    document.write('<br>El ' + numeroA + ' es divisible por 5.');
+} else if (numeroA % 7 === 0) {
+    document.write('<br>El ' + numeroA + ' es divisible por 7.');
 }else{
     document.write('<br>El numero ingresado: ' + numeroA + ' ,no es divisible por 7, 5, 3 y 2.');
 }
