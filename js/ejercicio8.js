3b2652e61c556a45b81afc1efa82dbde74a24566
Prado Nicolas <nicolas88prado@gmail.com>
2023-12-27 11:19:20 -0300

    Realice el ejercicio N8 del TP

---
+++ js/ejercicio8.js
@@ -0,0 +1,7 @@
+let numeroA = parseInt(prompt('Ingrese un valor numerico: '));
+
+if(numeroA % 2 == 0){
+    document.write('El numero ' + numeroA + ' es divisible por 2');
+}else{
+    document.write('El numero ' + numeroA + ' no es divisible por 2');
+}
